fe42cea8ffc401a2b86e769331665478eecbde94
wbarne12 <160870013+wbarne12@users.noreply.github.com>
2026-03-24 22:11:58 -0400

    Get file and parse args

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # ece365_lab2
-Repo for lab 2
+To run: "python3 cache_sim.py --sets SETS --blocks BLOCKS --size SIZE --trace TRACE"
+
+## Arg description
+Sets        (int): Number of sets in the cache.  
+Size        (int): Block size in bytes.  
+Trace (file name): File with memory addresses in hex.  
+Blocks      (int): Number of blocks per set  (associativity).  
+    *If blocks=1, the cache will be direct mapped*  
+
+## File requirements
+For the program to properly run, each memory address must be in hex format ie 0x1234ABCD.  
+Each address must be seperated by a new line character. Look at test.txt for an example.
---
+++ cache_sim.py
@@ -0,0 +1,55 @@
+import argparse
+import sys
+
+# Returns a list with all values formatted as ints
+# Conversion from hex to int performed
+def read_file(trace: str) -> list:
+    with open(trace, "r") as file:
+        return [int(line.strip(), 16) for line in file]
+
+# Reads the command line and puts args into respective data section.
+# Will handle all the wacky stuff that goes on with arg parsing.
+def parse_args() -> argparse.Namespace:
+    parser: argparse.ArgumentParser = argparse.ArgumentParser(description ="A program to simulate a cache.")
+
+    # TODO: Add --policy or --prefetch. Will depend on what we choose to implement.
+    parser.add_argument("--sets", type=int, required=True)
+    parser.add_argument("--blocks", type=int, required=True)
+    parser.add_argument("--size", type=int, required=True)
+    parser.add_argument("--trace", type=str, required=True)
+
+    return parser.parse_args()
+
+#https://www.geeksforgeeks.org/dsa/program-to-find-whether-a-given-number-is-power-of-2/
+def is_power_of_two(n):
+    return (n > 0) and ((n & (n - 1)) == 0)
+
+# Just checks if args are a power of two
+def error_check(args: argparse.Namespace):
+    is_power_two: bool = is_power_of_two(args.sets) & is_power_of_two(args.size) & is_power_of_two(args.blocks)
+    if (is_power_two == False):
+        sys.exit("Error: Arguments are not a power of two")
+    
+
+# Main execution
+if (__name__ == "__main__"):
+    args: argparse.Namespace = parse_args()
+    error_check(args)
+
+    sets:   int = args.sets
+    blocks: int = args.blocks
+    size:   int = args.size 
+    trace:  str = args.trace
+
+    address_list = read_file(trace)
+
+    # Can now get value of args using args.sets, args.blocks, args.size, args.trace
+    total_blocks: int = sets * blocks
+    cache_size: int = total_blocks * size
+
+    # Asked chat for good data structure and it gave this for the cache
+    cache = [[ {"tag": None, "valid": False} for _ in range(blocks) ] for _ in range(sets)]
+
+    #OFFSET bits = log2(size)
+    #INDEX bits = log2(sets)
+    #TAG = remaining bits
